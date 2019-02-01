91ae1dcda07ce9ba0ca44c4c36145c53c93b0572
AKD47 <van83horn@gmail.com>
2019-02-01 15:38:52 +0300

    start vue

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,60 +1,53 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="container-fluid navbar-inverse bg-inverse">
+      <div class="row">
+        <div class="col">
+          <nav class="navbar">
+            <form class="form-inline d-flex justify-content-end">
+              <div>
+                <button class="btn btn-outline-success mr-3" type="button" @click="switchSign('sign-in')">Войти</button>
+                <button class="btn btn-outline-success" type="button" @click="switchSign('sign-up')">Регистрация</button>
+              </div>
+            </form>
+          </nav>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <sign-in v-if="sign === 'sign-in'"></sign-in>
+          <sign-up v-else></sign-up>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import SignIn from './components/SignIn.vue'
+  import SignUp from './components/SignUp.vue'
+
+  export default {
+    name: 'app',
+    data() {
+      return {
+        sign: 'sign-in'
+      }
+    },
+    components: {
+      SignIn,
+      SignUp
+    },
+    methods: {
+      switchSign(currentSign) {
+        this.sign = currentSign;
+      }
     }
   }
-}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
